220585942bb016c04145f64cea71da722d230d02
Donnie Clapp <dhclapp@gmail.com>
2009-04-28 09:07:25 -0700

    just changed the spacing of the comment hash marks on the entries controller

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -4,44 +4,44 @@ class EntriesController < ApplicationController
   end
   
   # def show
-    #   @entry = Entry.find(params[:id])
-    # end
-    
+  #   @entry = Entry.find(params[:id])
+  # end
+  #   
   # def new
-    #   @entry = Entry.new
-    # end
-    # 
-    # def create
-    #   @entry = Entry.new(params[:entry])
-    #   if @entry.save
-    #     flash[:notice] = "Successfully created entry."
-    #     redirect_to @entry
-    #   else
-    #     render :action => 'new'
-    #   end
-    # end
-    # 
-    # def edit
-    #   @entry = Entry.find(params[:id])
-    # end
-    # 
-    # def update
-    #   @entry = Entry.find(params[:id])
-    #   if @entry.update_attributes(params[:entry])
-    #     flash[:notice] = "Successfully updated entry."
-    #     redirect_to @entry
-    #   else
-    #     render :action => 'edit'
-    #   end
-    # end
-    # 
-    # def destroy
-    #   @entry = Entry.find(params[:id])
-    #   @entry.destroy
-    #   flash[:notice] = "Successfully destroyed entry."
-    #   redirect_to entries_url
-    # end
-    
+  #   @entry = Entry.new
+  # end
+  # 
+  # def create
+  #   @entry = Entry.new(params[:entry])
+  #   if @entry.save
+  #     flash[:notice] = "Successfully created entry."
+  #     redirect_to @entry
+  #   else
+  #     render :action => 'new'
+  #   end
+  # end
+  # 
+  # def edit
+  #   @entry = Entry.find(params[:id])
+  # end
+  # 
+  # def update
+  #   @entry = Entry.find(params[:id])
+  #   if @entry.update_attributes(params[:entry])
+  #     flash[:notice] = "Successfully updated entry."
+  #     redirect_to @entry
+  #   else
+  #     render :action => 'edit'
+  #   end
+  # end
+  # 
+  # def destroy
+  #   @entry = Entry.find(params[:id])
+  #   @entry.destroy
+  #   flash[:notice] = "Successfully destroyed entry."
+  #   redirect_to entries_url
+  # end
+        
   def vote
     @entry = Entry.find(params[:id])
     @robot = Robot.new
