59bc92d1607f222fac1580b8fc28b40e69a97a61
Donnie Clapp <dhclapp@gmail.com>
2009-04-28 12:38:35 -0700

    added edit functionality back in for the meantime; removed entry 26 via data dump and load

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -3,44 +3,44 @@ class EntriesController < ApplicationController
     @entries = Entry.all
   end
   
-  # def show
-  #   @entry = Entry.find(params[:id])
-  # end
-  #   
-  # def new
-  #   @entry = Entry.new
-  # end
-  # 
-  # def create
-  #   @entry = Entry.new(params[:entry])
-  #   if @entry.save
-  #     flash[:notice] = "Successfully created entry."
-  #     redirect_to @entry
-  #   else
-  #     render :action => 'new'
-  #   end
-  # end
-  # 
-  # def edit
-  #   @entry = Entry.find(params[:id])
-  # end
-  # 
-  # def update
-  #   @entry = Entry.find(params[:id])
-  #   if @entry.update_attributes(params[:entry])
-  #     flash[:notice] = "Successfully updated entry."
-  #     redirect_to @entry
-  #   else
-  #     render :action => 'edit'
-  #   end
-  # end
-  # 
-  # def destroy
-  #   @entry = Entry.find(params[:id])
-  #   @entry.destroy
-  #   flash[:notice] = "Successfully destroyed entry."
-  #   redirect_to entries_url
-  # end
+  def show
+    @entry = Entry.find(params[:id])
+  end
+    
+  def new
+    @entry = Entry.new
+  end
+  
+  def create
+    @entry = Entry.new(params[:entry])
+    if @entry.save
+      flash[:notice] = "Successfully created entry."
+      redirect_to @entry
+    else
+      render :action => 'new'
+    end
+  end
+  
+  def edit
+    @entry = Entry.find(params[:id])
+  end
+  
+  def update
+    @entry = Entry.find(params[:id])
+    if @entry.update_attributes(params[:entry])
+      flash[:notice] = "Successfully updated entry."
+      redirect_to @entry
+    else
+      render :action => 'edit'
+    end
+  end
+  
+  def destroy
+    @entry = Entry.find(params[:id])
+    @entry.destroy
+    flash[:notice] = "Successfully destroyed entry."
+    redirect_to entries_url
+  end
         
   def vote
     @entry = Entry.find(params[:id])
